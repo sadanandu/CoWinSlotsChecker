6710f320d752371914842c117715035540128591
Sadanand Upase <sadu1437@gmail.com>
2021-05-04 22:18:18 +0530

    Changing  the date strategy
    Changing  the date strategy as data provided for individual dates is not accurate , so following what the cowin website follows.

--- check_cowin_slots.py
+++ check_cowin_slots.py
@@ -3,43 +3,46 @@ import datetime
 import json
 import time
 def get_slots(date, pincode):
-    resp = requests.get(
-        "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={}&date={}".format(pincode, date))
-    time.sleep(1)
+    url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={}&date={}".format(pincode, date)
+    resp = requests.get(url)
+    #time.sleep(1)
     json_data = json.loads(resp.text)
     center = []
+    print("Received for %s: "%url + str(json_data))
     for i in json_data["centers"]:
         for each in i["sessions"]:
-            if each["min_age_limit"] in (18, '18') and each["available_capacity"] > 0:
-                center.append(i)
-                break
-    if center:
-        return str(center)
-    else:
-        return None
+            if int(each["available_capacity"]) > 0 and datetime.datetime.strptime(each["date"], "%d-%m-%Y").date() >= datetime.date.today():
+                print("Got a session")
+                return (each["date"], pincode)
+
+
 
 def lambda_handler(event, context):
     # TODO implement
     
     reply = []
-    for i in range(7):
-        d2 = datetime.date.today() + datetime.timedelta(i)
+    for i in [2,9]:
+        d1 = datetime.date(month=5,day=i,year=2021)
+        #d2 = d1 + datetime.timedelta(i)
+        d2 = d1
         try:
-            pincode = '413531'
-            centers = get_slots(d2.strftime("%d-%m-%Y"), pincode)
-            if centers:
-                reply.append(centers)
-            pincode = '413512'
-            centers = get_slots(d2.strftime("%d-%m-%Y"), pincode)
-            if centers:
-                reply.append(centers)
-            
+            pincodes = ['431517']
+            for pincode in pincodes:
+                got_slot = get_slots(d2.strftime("%d-%m-%Y"), pincode)
+                if got_slot:
+                    return {
+                        'statusCode': 200,
+                        'body': json.dumps(str(got_slot))
+                    }
         except Exception as e:
-            print("Error occurred for fetching for %s" % d2.strftime("%d-%m-%Y") )
-    if reply:
-        return {
-            'statusCode': 200,
-            'body': json.dumps('\n'.join(reply))
-        }
-    
-    
+            print("Error occurred for fetching for %s: %s" % (d2.strftime("%d-%m-%Y"), str(e)) )
+    raise Exception("No slots")
+                
+            # pincode = '413512'
+            # got_slot = get_slots(d2.strftime("%d-%m-%Y"), pincode)
+            # if got_slot:
+            #     return {
+            #         'statusCode': 200,
+            #         'body': json.dumps(str(got_slot))
+            #     }
+            
